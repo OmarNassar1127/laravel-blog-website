67005d847ac6ee28db7b82450064c93e54ea58a1
Omar Nassar <omar.portero.nassar1@gmail.com>
2023-06-29 15:14:10 +0200

    yes

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -10,29 +10,29 @@ class FollowController extends Controller
 {
     //
     public function createFollow(User $user)
-{
-    // Check if the user is trying to follow themselves
-    if ($user->id == auth()->user()->id) {
-        return back()->with('error', 'You cannot follow yourself');
-    }
-    
-    // Check if the user is already following the specified user
-    $existCheck = Follow::where([
-        ['user_id', '=', auth()->user()->id],
-        ['followeduser', '=', $user->id]
-    ])->count();
-    if ($existCheck) {
-        return back()->with('error', 'You are already following this user');
-    }
+    {
+        // Check if the user is trying to follow themselves
+        if ($user->id == auth()->user()->id) {
+            return back()->with('error', 'You cannot follow yourself');
+        }
+        
+        // Check if the user is already following the specified user
+        $existCheck = Follow::where([
+            ['user_id', '=', auth()->user()->id],
+            ['followeduser', '=', $user->id]
+        ])->count();
+        if ($existCheck) {
+            return back()->with('error', 'You are already following this user');
+        }
 
-    // Create a new Follow model instance
-    $newFollow = new Follow;
-    $newFollow->user_id = auth()->user()->id; // Set the authenticated user as the follower
-    $newFollow->followeduser = $user->id; // Set the specified user as the followed user
-    $newFollow->save(); // Save the new follow record
+        // Create a new Follow model instance
+        $newFollow = new Follow;
+        $newFollow->user_id = auth()->user()->id; // Set the authenticated user as the follower
+        $newFollow->followeduser = $user->id; // Set the specified user as the followed user
+        $newFollow->save(); // Save the new follow record
 
-    return back()->with('success', 'You are now following this user');
-}
+        return back()->with('success', 'You are now following this user');
+    }
 public function removeFollow(User $user)
 {
     // Find and delete the follow relationship between the authenticated user and the specified user
